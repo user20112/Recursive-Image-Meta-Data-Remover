851b9ad667ff276f07875e341e2683e679b20800
dalara878 <devlinalara@gmail.com>
2023-06-16 16:32:31 -0400

    Super simple app that just goes through and uses skiasharp to decode an image then re encode it to delete any comment blocks in the file.

---
+++ Recursive Image Metadata Remover/Program.cs
@@ -0,0 +1,30 @@
+﻿using SkiaSharp;
+
+string executableLocation = AppContext.BaseDirectory;
+List<string> images = new List<string>();
+RecursiveGetAllPngImages(executableLocation, images);
+foreach (string imageFile in images)
+{
+    try
+    {
+        using SKImage image = SKImage.FromEncodedData(File.ReadAllBytes(imageFile));
+        File.WriteAllBytes(imageFile, image.Encode(SKEncodedImageFormat.Png, 100).ToArray()); 
+        Console.WriteLine($"Decoded and recoded image file :{imageFile}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Had an issue with the file :{imageFile}\r\n is this a correctly formated png ? \r\n {ex.Message}");
+    }
+}
+Console.WriteLine("Finished all found files, press enter to exit");
+Console.ReadLine();
+
+
+
+void RecursiveGetAllPngImages(string directory, List<string> imageList)
+{
+    imageList.AddRange(Directory.EnumerateFiles(directory, "*.png"));
+    IEnumerable<string> subDirectories = Directory.EnumerateDirectories(directory);
+    foreach (string subDirectory in subDirectories)
+        RecursiveGetAllPngImages(subDirectory, imageList);
+}
